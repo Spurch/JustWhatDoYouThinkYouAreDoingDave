cbabe4beb1d5b20c889c551b1ac2bbc236a640b8
iliantova <iliyana.antova@gmail.com>
2015-12-06 16:34:56 +0200

    beginning

---
+++ HAL/HAL9000/HALLogic/HALLGameLogic.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace HAL9000
+{
+    using System;
+    using Santase.Logic.Players;
+
+    public partial class HAL9000
+    {
+        private PlayerAction FirstStepState(PlayerTurnContext context)
+        {
+            if (context.IsFirstPlayerTurn)
+            {
+                
+            }
+            else
+            {
+                
+            }
+            return null;
+        }
+    }
+}
