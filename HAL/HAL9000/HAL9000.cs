ea0d1f9f92f21629f675713e7331248ab043ab7f
Ivan Nikolov <ivan.stoyanov.nikolov@gmail.com>
2015-12-06 14:24:42 +0200

    Initial submit!

---
+++ HAL/HAL9000/HAL9000.cs
@@ -0,0 +1,40 @@
+﻿namespace HAL9000
+{
+    using Santase.Logic.Players;
+    using System.Collections.Generic;
+    using Santase.Logic;
+    using Santase.Logic.Cards;
+
+    public partial class HAL9000: BasePlayer
+    {
+        private readonly string name = "HAL9000";
+
+
+        public override PlayerAction GetTurn(PlayerTurnContext context)
+        {
+            if (this.PlayerActionValidator.IsValid(PlayerAction.ChangeTrump(), context, this.Cards))
+            {
+                return this.ChangeTrump(context.TrumpCard);
+            }
+
+            if (this.CloseGame(context))
+            {
+                return this.CloseGame();
+            }
+
+            return null;
+        }
+
+        public override string Name
+        {
+            get { return this.name; }
+        }
+
+        //TODO: Create closing decision making logic.
+        private bool CloseGame(PlayerTurnContext context)
+        {
+            
+            return true;
+        }
+    }
+}
---
+++ HAL/HAL9000/HALLogic/Common.cs
@@ -0,0 +1,32 @@
+﻿namespace HAL9000
+{
+    using System.Collections.Generic;
+    using Santase.Logic;
+    using Santase.Logic.Cards;
+    using Santase.Logic.Players;
+
+    public partial class HAL9000
+    {
+        /// <summary>
+        /// Method that checks if we can announce 20 or 40 and returns a list of the queens that we
+        /// can use for possible announces.
+        /// </summary>
+        /// <param name="context">The current PlayerTurnContext context</param>
+        /// <param name="currentPossibleCardsToPlay">The current possible cards to play.</param>
+        /// <returns>A list of type Card that contains all possible queens to announce - if any, else returns an empty list.</returns>
+        public List<Card> CheckForTwentyOrForty(PlayerTurnContext context, ICollection<Card> currentPossibleCardsToPlay)
+        {
+            var queensList = new List<Card>();
+            foreach (var currentCard in currentPossibleCardsToPlay)
+            {
+                if (currentCard.Type == CardType.Queen
+                    && (this.AnnounceValidator.GetPossibleAnnounce(this.Cards, currentCard, context.TrumpCard) == Announce.Twenty
+                    || this.AnnounceValidator.GetPossibleAnnounce(this.Cards, currentCard, context.TrumpCard) == Announce.Forty))
+                {
+                    queensList.Add(currentCard); 
+                }
+            }
+            return queensList;
+        }
+    }
+}
